2c7c331c4759a4487dbe4527a0dcdc01cc810156
A-Ladin <109243154+A-Ladin@users.noreply.github.com>
2022-09-16 15:59:56 +0300

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -1,11 +1,21 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
-path = '/home/a_ladin/ds-phase-0/learning/datasets/tips.csv'
-tips = pd.read_csv(path, index_col=0)
-
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, 'rb') as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f'''
+    <style>
+    .stApp {{
+        background-image: url(data:image/{'png'};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    ''',
+    unsafe_allow_html=True
+    )
+add_bg_from_local('generator.jpeg')
 
 def main_page():
     st.markdown("# Main page")
