f9c2708a18717c69e51d8b318014b93cd7743657
A-Ladin <109243154+A-Ladin@users.noreply.github.com>
2022-09-16 15:59:02 +0300

    Add files via upload

---
+++ pages/Оценю отзыв.py
@@ -0,0 +1,252 @@
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from joblib import load
+import re
+import string
+import torch
+import torch.nn as nn
+from json import load as json_load
+import transformers
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.colors as mcolors
+from PIL import Image
+
+path_folder = '/home/a_ladin/ds_offline/learning/project2/review_files/'
+
+# Возможно нужны будут
+nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
+## -------------------------- Для 1-ой модели ---------------------------
+
+# Кэшируем стоп-слова
+sw = stopwords.words('english')
+# Загружаем vectorizer
+tfid = load(path_folder + 'tfidf.joblib')
+# Загружаем модель для sentiment_ML
+model_ml = load(path_folder + 'logreg.joblib')
+
+
+def clean(text):
+    "Функция чистит текст"
+    text = text.lower()  # нижний регистр
+    text = re.sub('<.*?>', '', text)  # Remove HTML from text
+    text = text.translate(str.maketrans('', '', string.punctuation))  # удаляем знаки препинания
+    text = re.sub(r'\d+', ' ', text)  # удаляем числа
+
+    return text
+
+
+def tokenize_review(review: str):
+    "Функция чистит текст и возвращает токенизированные слова"
+    cleaned_review = clean(review)  # cleaning text
+    wn_lemmatizer = WordNetLemmatizer()  # lemmatization
+    reg_tokenizer = RegexpTokenizer('\w+')  # tokenization
+
+    lemmatized_review = ' '.join([wn_lemmatizer.lemmatize(word, tag[0].lower())  # лемматизация с учётом части речи
+                                  if tag[0].lower() in ['a', 'n', 'v']  # word - проверка на adv, noun и verb
+                                  else wn_lemmatizer.lemmatize(word)  # простая лемматизация
+                                  for word, tag in
+                                  nltk.pos_tag(cleaned_review.split())])  # pos_tag - определяет часть речи
+
+    tokenized_review = reg_tokenizer.tokenize_sents([lemmatized_review])  # ревью состоит из токенов
+    clean_tokenized_review = ' '.join(
+        [word for word in tokenized_review[0] if word not in sw])  # удаляем стоп-слова, сверху объявили sw
+
+    return clean_tokenized_review
+
+
+## -------------------------- Для 2-ой модели ---------------------------
+
+class sentimentGRU(nn.Module):
+    def __init__(self,
+                 vocab_size,  # объём словаря слов
+                 output_size,  # нейроны полносвязного слоя
+                 embedding_dim,  # размер выходного эмбеддинга
+                 hidden_dim,  # размерность внутреннего слоя LSTM
+                 n_layers,  # число слоев в LSTM
+                 drop_prob=0.5):
+        super().__init__()
+
+        self.output_size = output_size
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+        self.gru = nn.GRU(embedding_dim,
+                          hidden_dim,
+                          n_layers,
+                          dropout=drop_prob,
+                          batch_first=True)
+
+        self.dropout = nn.Dropout(0.3)
+        self.fc = nn.Linear(hidden_dim, output_size)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, hidden):
+        batch_size = x.size(0)
+        embeds = self.embedding(x)
+
+        gru_out, hidden = self.gru(embeds, hidden)
+        gru_out = gru_out.contiguous().view(-1, self.hidden_dim)
+
+        out = self.dropout(gru_out)
+        out = self.fc(out)
+
+        sig_out = self.sigmoid(out)
+        sig_out = sig_out.view(batch_size, -1)
+        sig_out = sig_out[:, -1]
+
+        return sig_out, hidden
+
+    def init_hidden(self, batch_size):
+        "Hidden state инициализируем нулями"
+        hidden = torch.zeros((self.n_layers), batch_size, self.hidden_dim)
+
+        return hidden
+
+
+# загружаем словарь слово-число
+with open(path_folder + 'dict.json', 'r') as fp:
+    vocab_to_int = json_load(fp)
+
+# загружаем модель для sentiment_RNN
+vocab_size = len(vocab_to_int) + 1  # размер словаря vocab_to_int
+output_size = 1  # задача бинарной классификации
+embedding_dim = 32  # размер слова
+hidden_dim = 16  # размер вектора истории
+n_layers = 2  # количество GRU слоев
+
+model_rnn = sentimentGRU(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
+model_rnn.load_state_dict(torch.load(path_folder + 'params_gru.pt', map_location=torch.device('cpu')))
+model_rnn.eval()
+
+## -------------------------- Для 3-ой модели ---------------------------
+
+
+# DistilBERT: ## задаем саму модель
+model_class = transformers.DistilBertModel
+## токенайзер к ней (для некоторых моделей токенайзер будет отличаться, см. в документации к каждой модели конкретно)
+tokenizer_class = transformers.DistilBertTokenizer
+
+## загружаем веса для моделей
+pretrained_weights = 'distilbert-base-uncased'
+tokenizer_bert = tokenizer_class.from_pretrained(pretrained_weights)
+model_bert = model_class.from_pretrained(pretrained_weights)
+model_bert_ml = load(path_folder + 'log_reg_2.joblib')
+
+
+# 1-ая модель
+def sentiment_ML(review: str):
+    "функция обрабатывает текст и предсказывает на ML-модели"
+
+    clean_tokenized_review = tokenize_review(review)
+    tfid_representation = tfid.transform(
+        [clean_tokenized_review])  # ревью состоит из векторов, сверху загружаем vectorizer
+
+    pred_proba = model_ml.predict_proba(tfid_representation)  # хранит список предсказаний для двух классов
+
+    return np.round(pred_proba[0][1], 3)  # берем первое и единственное предсказание и вероятность 1 класса (позитивный)
+
+
+# 2-ая модель
+def sentiment_RNN(review: str):
+    "функция обрабатывает текст и предсказывает на RNN-модели"
+
+    clean_tokenized_review = tokenize_review(review)
+
+    num_review = []  # здесь будут вместо слов числа из словаря vocab_to_int #векторизуем
+    for word in clean_tokenized_review.split():
+        try:
+            num_review.append(vocab_to_int[word])
+        except KeyError as e:
+            print(f'Word {word} not in dictionary!')
+
+    # padding
+    padding_review = num_review[-200:]  # обрубаем если больше 200 слов
+    if len(num_review) <= 200:
+        padding_review = list(np.zeros(200 - len(num_review))) + num_review  # дополняем нулями если меньше 200 слов
+
+    tensor_review = torch.Tensor(padding_review).long().unsqueeze(0)  # создаем тензор ревью для модели
+    test_h = model_rnn.init_hidden(1)  # создаем hidden_state
+
+    pred = model_rnn(tensor_review, test_h)
+    pred_proba = pred[0].item()
+
+    return np.round(pred_proba, 3)  # выдает вероятность 1 класса (позитивный)
+
+
+# 3-ая модель
+def sentiment_BERT_ML(review: str):
+    "функция обрабатывает текст с помощью BERT и предсказывает на ML-модели"
+    max_len = 64  # максимальная длина последовательности
+
+    # применяем токенизатор
+    tokenized = tokenizer_bert.encode(review,
+                                      add_special_tokens=True, truncation=True,
+                                      max_length=max_len)  # добавили служебные токены и обрезали по макс длине
+
+    padded = np.array(tokenized + [0] * (max_len - len(tokenized)))  # дополняем нулями если меньше 64 слов
+
+    attention_mask = np.where(padded != 0, 1, 0)  # маскирование
+
+    # переводим в тензоры
+    input_ids = torch.tensor(padded).unsqueeze(0)
+    attention_mask = torch.tensor(attention_mask).unsqueeze(0)
+
+    with torch.inference_mode():
+        last_hidden_states = model_bert(input_ids, attention_mask=attention_mask)
+
+    features = last_hidden_states[0][:, 0, :].numpy()
+
+    pred_proba = model_bert_ml.predict_proba(features)
+
+    return np.round(pred_proba[0][1], 3)  # берем первое и единственное предсказание и вероятность 1 класса (позитивный)
+
+
+movie_names = {'Зеленая книга': '2',
+               'Пролетая над гнездом кукушки': '1',
+               'Мстители: где Танос убил половину': '5',
+               'Сумерки: где Белла стала вампиром': '4',
+               'Ну, погоди!': '3'}
+
+
+st.set_page_config(
+    page_title="Film review predictions",
+    page_icon="👋",
+)
+
+st.title("Film review predictions")
+selected_movie = st.selectbox("Select movie", movie_names.keys())
+image = Image.open(path_folder + movie_names[selected_movie] + '.jpg')
+st.image(image, caption=selected_movie)
+my_input = st.text_input('Введите текст')
+if my_input:
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    clist = [(0, "red"), (0.125, "red"), (0.25, "orange"), (0.5, "green"),
+             (0.7, "green"), (0.75, "blue"), (1, "blue")]
+    rvb = mcolors.LinearSegmentedColormap.from_list("", clist)
+    model = ('Classic ML', 'LSTM', 'BERT')
+    y_pos = np.arange(len(model))
+    prob = np.array([sentiment_ML(my_input), sentiment_RNN(my_input), sentiment_BERT_ML(my_input)])
+
+    fig.set_figwidth(4)
+    fig.set_figheight(1)
+
+    ax.barh(y_pos, prob, align='center', color=rvb(prob))
+    ax.set_yticks(y_pos, labels=model)
+    ax.set_xlim(left=0, right=1)
+    ax.invert_yaxis()
+    ax.set_xlabel('Positivity')
+    ax.set_title('Model predictions')
+
+    st.pyplot(fig)
+
---
+++ pages/Сгенерирую текст.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import base64
+import torch
+import textwrap
+
+from transformers import GPT2Tokenizer
+from transformers import GPT2LMHeadModel
+tokenizer = GPT2Tokenizer.from_pretrained('sberbank-ai/rugpt3small_based_on_gpt2')
+model_init = GPT2LMHeadModel.from_pretrained(
+    'sberbank-ai/rugpt3small_based_on_gpt2',
+    output_attentions=False,
+    output_hidden_states=False, )
+weights_path = '/home/a_ladin/ds_offline/learning/project2/weights_of_preprocessing_text_Visotskii.pt'
+model_init.load_state_dict(torch.load(weights_path))
+
+
+def add_bg_from_local(image_file):
+    with open(image_file, 'rb') as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f'''
+    <style>
+    .stApp {{
+        background-image: url(data:image/{'png'};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    ''',
+    unsafe_allow_html=True
+    )
+add_bg_from_local('realistic-studio-lights-empty.jpg')
+
+
+streamlit_style = """
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+			html, body, [class*="css"]  {
+			font-family: 'Roboto', sans-serif;
+			font-size: 15px;
+    font-weight: 1000;
+    color: #091747;
+			}
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)
+
+st.snow()
+best_len = 100
+best_num_beams = 3
+best_temp = 2
+
+slider1 = st.slider('Длина предложения', 50, 200, best_len)
+slider2 = st.slider('num_beams', 0, 7, best_num_beams)
+slider3 = st.slider('Температура', 1, 10, best_temp)
+
+user_input = st.text_input("Напиши начало строки, все остальное я сделаю сама")
+
+
+def generation(user_input, len, beam, temp):
+    prompt = user_input
+    prompt = tokenizer.encode(prompt, return_tensors='pt')
+    out = model_init.generate(
+        input_ids=prompt,
+        max_length=len,
+        num_beams=beam,
+        do_sample=True,
+        temperature=float(temp),
+        top_k=10,
+        top_p=0.6,
+        no_repeat_ngram_size=3,
+        num_return_sequences=1,
+    ).cpu().numpy()
+
+
+    for out_ in out:
+        text_dirty = textwrap.fill(tokenizer.decode(out_), 120)
+    text_clear = text_dirty.split()
+    for i, word in enumerate(text_clear):
+        # the_end = i
+        if ('.' in word) or ('!' in word) or ('?' in word):
+            the_end = i + 1
+    return (' '.join(text_clear[:the_end]))
+
+if user_input:
+    st.write(generation(user_input, slider1, slider2, slider3))
